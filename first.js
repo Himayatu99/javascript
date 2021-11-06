0312fd5384b5220ffd91ecb91ab18c360f291a4e
khan <himayatu99@gmail.com>
2021-11-06 08:25:21 +0500

    add Higher order array method

--- first.js
+++ first.js
@@ -1,5 +1,12 @@
-const a = 10;
-const b = 50;
+// for(let i=0;i<list.length; i++) {
+//     console.log(i);
+// }
 
-const c = a + b;
-console.log(c);
+//Higher order array method
+
+const list = [29, 4, 56, 77, 54, 22];
+list.forEach(function (l, key, arry) {
+    console.log(l);
+    // console.log(key);
+    // console.log(arry);
+})
