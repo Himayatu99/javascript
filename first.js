229652ce5d30112b098db386311d9f3ed2279036
khan <himayatu99@gmail.com>
2021-11-06 09:28:52 +0500

    Add map, filter,reduce functions

--- first.js
+++ first.js
@@ -10,3 +10,18 @@ list.forEach(function (l, key, arry) {
     // console.log(key);
     // console.log(arry);
 })
+const newList = list.map(function (l) {
+    return l * l;
+})
+console.log(newList);
+
+// for filter
+const passed = list.filter(function (l) {
+    return l % 2 == 0;
+})
+console.log(passed);
+
+const redu = list.reduce(function (prev, curr) {
+    return prev + curr
+})
+console.log(redu);
