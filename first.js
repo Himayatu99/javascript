2dfacfae0ff81ff6d4a8533e71db55f24d94af93
khan <himayatu99@gmail.com>
2021-11-06 15:17:52 +0500

    Javascript opertors and its types

--- first.js
+++ first.js
@@ -1,13 +1,64 @@
-// for more easies method 
-list = [3, 4, 5, 22, 11, 99, 77]
+// Javascript opertors (+,-,/,*,%)
+//  Types of opertors 
+// 1) Arithmetic operator
+// Addition
+var x = 40;
+var y = 50;
+var z = x + y;
+console.log(z)
 
-const sum = list.map(function (m) {
-    return m * m * m;
-}).filter(function (f) {
-    return f > 50;
-}).reduce(function (prev, curr) {
-    return prev + curr;
-}, 0)
+//Subtraction
 
-console.log(sum)
+var x = 50;
+var y = 20;
+var z = x - y;
+console.log(z)
+
+// Moduls
+
+var x = 50;
+var y = 2;
+var z = x % y;
+console.log(z)
+
+// 2) Assignment opertor (=,+=.*=,/=)
+
+var a = 40;
+var b = 2;
+var c = a * b;
+console.log(c)
+
+// 3)comparison operators (==, !=,>,<=,<)
+
+number = 500;
+
+if (number == 500) {
+    console.log('passed');
+}
+else {
+    console.log("fail")
+
+}
+
+// 4)logical operators (&&, ||, !)
+// &&
+var x = true;
+var y = true;
+z = x && y
+console.log(z)
+
+// ||
+
+var c = true;
+var d = false;
+e = c || d;
+console.log(e)
+
+// 5) String :The collection of words is called String
+
+var FirstName = "Khan";
+var LastName = "jan";
+var z = FirstName.concat(" ", LastName);
+console.log(z)
+
 
