ee1172354762001d715d4e6c4ecca30e0816bbbc
khan <himayatu99@gmail.com>
2021-11-08 08:26:25 +0500

    Add  for loop ,while loop and do while loop

---
+++ loop.js
@@ -0,0 +1,23 @@
+// looping 
+
+// 1) For loop
+for (i = 1; i < 11; i++) {
+    console.log(i + " * 3= " + (i * 3))
+}
+
+// while loop
+
+i = 1;
+while (i < 11) {
+    console.log(i + " * 2= " + (i * 2))
+    i++;
+}
+
+// do while loop
+
+i = 12;
+do {
+    console.log("2 * " + i + "=" + (i * 2))
+    i++;
+}
+while (i < 11)
