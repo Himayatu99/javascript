b4136f6ae981863480d4e7e9b3512292b561d2cc
khan <himayatu99@gmail.com>
2021-11-08 11:23:42 +0500

    Add object

---
+++ object.js
@@ -0,0 +1,8 @@
+// object
+var person = {
+    first: "khan",
+    last: "jan",
+    age: "22"
+}
+document.getElementById('demo').innerHTML = "Dear" + person.first + person.last + person.age
+// alert(person.first + person.last + person.age)
