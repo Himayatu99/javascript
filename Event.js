d6f53570821dda03e455972fbf29a649d9133fbe
khan <himayatu99@gmail.com>
2021-11-09 21:02:50 +0500

    onclick,onload Event

---
+++ Event.js
@@ -0,0 +1,11 @@
+function welcome() {
+    alert("Welcome to our site")
+}
+
+function click() {
+    alert('Welcome to .......')
+}
+
+function down() {
+    document.getElementsByName('h2').style.color = "orange";
+}
