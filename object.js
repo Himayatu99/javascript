dc5948b7a47a164f60d1b56b0a565c8197cacf63
khan <himayatu99@gmail.com>
2021-11-09 21:34:18 +0500

    add  different Data object

--- object.js
+++ object.js
@@ -1,8 +1,15 @@
 // object
-var person = {
-    first: "khan",
-    last: "jan",
-    age: "22"
-}
-document.getElementById('demo').innerHTML = "Dear" + person.first + person.last + person.age
+// var person = {
+//     first: "khan",
+//     last: "jan",
+//     age: "22"
+// }
+// document.getElementById('demo').innerHTML = "Dear" + person.first + person.last + person.age
 // alert(person.first + person.last + person.age)
+
+// Data object
+var today = new Date();
+// console.log(today)
+var x = today.getDate()
+// get month, get minutes,get hours,seconds etc
+console.log(x)
