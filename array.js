cb7c46788c06c02fb5a58e114099f051f53b3f06
khan <himayatu99@gmail.com>
2021-11-10 21:54:24 +0500

    Add array

---
+++ array.js
@@ -0,0 +1,9 @@
+// Array 
+
+var city = ["peshawar", "Islamabad", "Lahor", "FSD"];
+console.log(city);
+
+var month = ["Jan", "Feb", "May"];
+var day = ["sun", "Mon", "friday"];
+document.write(month.concat(day));
+
